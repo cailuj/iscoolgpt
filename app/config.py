b28eeadf1d0dfacdb0d746e298db90befb43e5cd
cailuj <juliascalado.28@gmail.com>
2025-12-01 00:21:10 -0300

    fix: corrigido modelo para gemma 2b em config.py

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Settings:
     PROJECT_NAME: str = "IsCoolGPT - Gemma 2B API"
     HF_TOKEN: str | None = os.getenv("HF_TOKEN")
-    GEMMA_MODEL_NAME: str = os.getenv("GEMMA_MODEL_NAME", "google/gemma-3-4b-it")
+    GEMMA_MODEL_NAME: str = os.getenv("GEMMA_MODEL_NAME", "google/gemma-2b-it")
 
     MAX_NEW_TOKENS: int = int(os.getenv("MAX_NEW_TOKENS", "256"))
     TEMPERATURE: float = float(os.getenv("TEMPERATURE", "0.7"))
