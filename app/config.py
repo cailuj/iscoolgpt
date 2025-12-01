0f1acbcaa0cb8f173b98d77e0e67844777ec8ee6
cailuj <juliascalado.28@gmail.com>
2025-12-01 11:58:30 -0300

    ajuste na temperatura

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Settings:
     MODEL_NAME: str = os.getenv("MODEL_NAME", "google/flan-t5-small")
 
     MAX_NEW_TOKENS: int = int(os.getenv("MAX_NEW_TOKENS", "256"))
-    TEMPERATURE: float = float(os.getenv("TEMPERATURE", "0.1"))
+    TEMPERATURE: float = float(os.getenv("TEMPERATURE", "0.3"))
     TOP_P: float = float(os.getenv("TOP_P", "0.9"))
 
 settings = Settings()
